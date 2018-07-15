f5daaa2ebe1aa651321344a0f4f9a5656ceb8d16
jjavaboy <jjavaboy@163.com>
2018-07-15 20:52:33 +0800

    first commit.

---
+++ cobia-core/src/test/java/cobia/AppTest.java
@@ -0,0 +1,20 @@
+package cobia;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
